9301eb99c474305a07a17b1b8a91faed32fb837c
taegyu jang <taegyu1500@gmail.com>
2025-10-29 14:14:24 +0900

    ci test

--- parser.py
+++ parser.py
@@ -1,20 +1,11 @@
 from frontmatter import Frontmatter
 
 
-
-
-
-
-
 def parse_markdown(file_path):
     d = Frontmatter.read_file(file_path)
     if __name__ == "__main__":
         print(d["attributes"])
     return d["attributes"]
-
-
-
-
 
 
 if __name__ == "__main__":
